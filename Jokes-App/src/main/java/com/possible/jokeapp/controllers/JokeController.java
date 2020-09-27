d55dc3bf04d24273f47fd31a29ae7dddf9e06913
possible-gratitude <abayomi.gratitudeindia@gmail.com>
2020-09-27 13:46:09 +0100

    Joke Controller created

--- Jokes-App/src/main/java/com/possible/jokeapp/controllers/JokeController.java
+++ Jokes-App/src/main/java/com/possible/jokeapp/controllers/JokeController.java
@@ -1,4 +1,24 @@
 package com.possible.jokeapp.controllers;
 
+import com.possible.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @GetMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "jokes";
+    }
 }
